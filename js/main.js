5c7e5ff32784110b86efc7dfb575b877199beec1
realtytech <realtytechmumbai@gmail.com>
2022-01-28 08:46:25 +0530

    Rustomjee Uptown Urbania Added

--- js/main.js
+++ js/main.js
@@ -90,9 +90,9 @@ $("#leadForm-popup").submit(function (e) {
 
     // var srd = selectSRD(utm_source, utm_campaign);
     var srd = queryParameter('srd', currentUrl);
-    if (!srd) srd = '7015g0000004xf7RC';
+    if (!srd) srd = '7015g0000004xf7RUU';
 
-    var project = 'Rustomjee Crown';
+    var project = 'Rustomjee Uptown Urbania';
     var utm_source = queryParameter('utm_source',currentUrl);
     var utm_medium = queryParameter('utm_medium',currentUrl);
     var data = {
